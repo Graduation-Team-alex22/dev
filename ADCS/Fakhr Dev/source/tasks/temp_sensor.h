3b74f480f85f14d79266786bef43413fd8799b5c
ali <alihmdy98@gmail.com>
2022-03-16 17:36:01 +0200

    Update temp_sensor.h

--- ADCS/Fakhr Dev/source/tasks/temp_sensor.h
+++ ADCS/Fakhr Dev/source/tasks/temp_sensor.h
@@ -1,12 +1,15 @@
 #ifndef __TEMP_SENSOR_H__
-#define __TEMP_SENSOR_H__
+#define __TEMP_SENSOR_H__ 1
 
 #include <stdint.h>
 
 #define TEMP_SENSOR_ADDRESS		(0x18<<1)
 #define TEMP_REG_ADDRESS			(0x05)
 
+
+// ------ Public function prototypes (declarations) --------------------------
 void TEMP_SENSOR_Init(void);
 uint32_t TEMP_SENSOR_update(void);
 float TEMP_SENSOR_Get_Value(void);
+
 #endif
