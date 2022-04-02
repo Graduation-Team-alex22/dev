49ec41e629b8103a45161c735fa3d810dc05b991
Abdelrahman-sh7 <53278943+Abdelrahman-sh7@users.noreply.github.com>
2022-04-03 01:58:38 +0200

    importing the whole hsi lib

---
+++ EPS/Testing Project/Test3/TEST/Objects/main.d
@@ -0,0 +1,31 @@
+.\objects\main.o: main.c
+.\objects\main.o: .\hsi_library\stm32f4xx.h
+.\objects\main.o: .\hsi_library\core_cm4.h
+.\objects\main.o: E:\Program Files\New Folder\keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: .\hsi_library\core_cmInstr.h
+.\objects\main.o: .\hsi_library\core_cmFunc.h
+.\objects\main.o: .\hsi_library\core_cmSimd.h
+.\objects\main.o: .\hsi_startup\system_stm32f4xx.h
+.\objects\main.o: .\hsi_library\stm32f4xx_conf.h
+.\objects\main.o: .\hsi_library\stm32f4xx_adc.h
+.\objects\main.o: .\hsi_library\stm32f4xx.h
+.\objects\main.o: .\hsi_library\stm32f4xx_crc.h
+.\objects\main.o: .\hsi_library\stm32f4xx_dbgmcu.h
+.\objects\main.o: .\hsi_library\stm32f4xx_dma.h
+.\objects\main.o: .\hsi_library\stm32f4xx_exti.h
+.\objects\main.o: .\hsi_library\stm32f4xx_flash.h
+.\objects\main.o: .\hsi_library\stm32f4xx_gpio.h
+.\objects\main.o: .\hsi_library\stm32f4xx_i2c.h
+.\objects\main.o: .\hsi_library\stm32f4xx_iwdg.h
+.\objects\main.o: .\hsi_library\stm32f4xx_pwr.h
+.\objects\main.o: .\hsi_library\stm32f4xx_rcc.h
+.\objects\main.o: .\hsi_library\stm32f4xx_rtc.h
+.\objects\main.o: .\hsi_library\stm32f4xx_sdio.h
+.\objects\main.o: .\hsi_library\stm32f4xx_spi.h
+.\objects\main.o: .\hsi_library\stm32f4xx_syscfg.h
+.\objects\main.o: .\hsi_library\stm32f4xx_tim.h
+.\objects\main.o: .\hsi_library\stm32f4xx_usart.h
+.\objects\main.o: .\hsi_library\stm32f4xx_wwdg.h
+.\objects\main.o: .\hsi_library\misc.h
+.\objects\main.o: E:\Program Files\New Folder\keil\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\main.o: E:\Program Files\New Folder\keil\ARM\ARMCC\Bin\..\include\string.h
---
+++ EPS/Testing Project/Test3/TEST/Objects/startup_stm32f446xx_1.d
@@ -0,0 +1,1 @@
+.\objects\startup_stm32f446xx_1.o: RTE\Device\STM32F446RETx\startup_stm32f446xx.s
